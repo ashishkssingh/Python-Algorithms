353ec48498fe41267bca5d8c7ba944a7bcec1972
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2018-12-15 09:59:24 -0500

    Added comments to bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -11,19 +11,27 @@
 #
 #**********************************************************************;
 
+# Function that implements Bubble sort
 def bubbleSort(unsortedArray):
 
+	# get length of the array
 	arrayLength = len(unsortedArray)
 
+	# Loop throught all elements of the array
 	for i in range(arrayLength):
 
+		# inner loop to compare with every other elements
 		for j in range(arrayLength - i - 1):
 
+			# If the next values is less than previous, interchange them
 			if unsortedArray[j] > unsortedArray[j+1]:
+
 				unsortedArray[j],unsortedArray[j+1] = unsortedArray[j+1],unsortedArray[j]
 
+	# Call printArray function to print the sorted array
 	printArray(unsortedArray)
 
+# Function to print Sorted Array
 def printArray(sortedArray):
 
 	string = ""
@@ -31,10 +39,14 @@ def printArray(sortedArray):
 	for i in range(len(sortedArray)):
 		string += str(sortedArray[i]) + " "
 
+	# Print Sorted array to the console
 	print(string)
 
+# Main function that executes when script runs on the console
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
 
+	# Test values for sorting
     unsortedArray = [64, 34, 25, 12, 22, 11, 90]
+
+    # Calling bubble sort function
     bubbleSort(unsortedArray)
