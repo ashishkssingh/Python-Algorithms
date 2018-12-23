8f73597ab575c1cb444a365597fd9c658ca4e9c8
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2018-12-23 16:02:49 -0500

    Implementation of Jump Search in python 3

--- Searching Algorithms/JumpSearch.py
+++ Searching Algorithms/JumpSearch.py
@@ -11,44 +11,42 @@
 #
 # **********************************************************************;
 
-# Import libraries used
+# Import libraries that are being used
 import math
 
 # Function that implements the search algorithm
 def jumpSearch(searchArray, searchedValue, arraySize):
 
     # Finding block size to be jumped
-    step = math.sqrt(arraySize)
+    block = math.sqrt(arraySize)
 
-    # Finding the block where element is
-    # present (if it is present)
+    # Look for the block in which searched element is present
     prev = 0
-    while searchArray[int(min(step, arraySize) - 1)] < searchedValue:
-        prev = step
-        step += math.sqrt(arraySize)
+    while searchArray[int(min(block, arraySize) - 1)] < searchedValue:
+        prev = block
+        block += math.sqrt(arraySize)
         if prev >= arraySize:
             return -1
 
-    # Doing a linear search for searchedValue in
-    # block beginning with prev.
+    # Do a linear search in the block found in previous step
     while searchArray[int(prev)] < searchedValue:
         prev += 1
 
-        # If we reached next block or end
-        # of array, element is not present.
-        if prev == min(step, arraySize):
+        # If we reach the end of the block and don't find the element return -1 as index
+        if prev == min(block, arraySize):
             return -1
 
-    # If element is found
+    # If element is found in the given block return the value of the index
     if searchArray[int(prev)] == searchedValue:
         return int(prev)
-
+    
+    # If the element is still not found return -1 as index     
     return -1
 
 # Function to print index of the element
 def printIndex(index):
     
-    # Check for the value of the index
+    # Check for the value of the index, if -1 the value was not found else print the value of index
     if index == -1:
         print("Value was not found in the array")
     else:
