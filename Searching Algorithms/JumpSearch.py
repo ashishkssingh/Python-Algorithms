7f0c185e88f5a23fb729ad2f13ce0bffa0dd4047
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2018-12-23 15:54:53 -0500

    Implementation of Jump Search in python 3

--- Searching Algorithms/JumpSearch.py
+++ Searching Algorithms/JumpSearch.py
@@ -15,38 +15,39 @@
 import math
 
 # Function that implements the search algorithm
-def jumpSearch(arr, x, n):
+def jumpSearch(searchArray, searchedValue, arraySize):
 
     # Finding block size to be jumped
-    step = math.sqrt(n)
+    step = math.sqrt(arraySize)
 
     # Finding the block where element is
     # present (if it is present)
     prev = 0
-    while arr[int(min(step, n) - 1)] < x:
+    while searchArray[int(min(step, arraySize) - 1)] < searchedValue:
         prev = step
-        step += math.sqrt(n)
-        if prev >= n:
+        step += math.sqrt(arraySize)
+        if prev >= arraySize:
             return -1
 
-    # Doing a linear search for x in
+    # Doing a linear search for searchedValue in
     # block beginning with prev.
-    while arr[int(prev)] < x:
+    while searchArray[int(prev)] < searchedValue:
         prev += 1
 
         # If we reached next block or end
         # of array, element is not present.
-        if prev == min(step, n):
+        if prev == min(step, arraySize):
             return -1
 
     # If element is found
-    if arr[int(prev)] == x:
+    if searchArray[int(prev)] == searchedValue:
         return int(prev)
 
     return -1
 
 # Function to print index of the element
 def printIndex(index):
+    
     # Check for the value of the index
     if index == -1:
         print("Value was not found in the array")
@@ -55,6 +56,7 @@ def printIndex(index):
 
 # Main function that executes when script runs on the console.
 if __name__ == "__main__":
+    
     # Initialize searching array
     searchArray = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
 
