edf7d4a3589cc6496bcdd3d8606900af820640a5
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2019-01-09 14:22:24 -0500

    Implementation of bubble sort in python 3

--- Sorting Algorithms/BubbleSort.py
+++ Sorting Algorithms/BubbleSort.py
@@ -1,52 +1,59 @@
 # **********************************************************************;
 # Project           : Learning Algorithms
-# 
+#
 # Program name      : BubbleSort.py
 #
-# Author            : Ashish
+# Author            : Ashish Singh
 #
-# Date created      : 20181215
+# Date created      : 20190109
 #
 # Purpose           : Implementing bubble sort algorithm in python
 #
-#**********************************************************************;
+# **********************************************************************;
 
 # Function that implements Bubble sort
-def bubbleSort(unsortedArray):
+def bubbleSort(unsortedArray, sortingType):
 
-	# get length of the array
-	arrayLength = len(unsortedArray)
+    # get length of the array
+    arrayLength = len(unsortedArray)
 
-	# Loop throught all elements of the array
-	for i in range(arrayLength):
+    # Loop through all elements of the array
+    for i in range(arrayLength):
 
-		# inner loop to compare with every other elements
-		for j in range(arrayLength - i - 1):
+        # inner loop to compare with every other elements
+        for j in range(arrayLength - i - 1):
 
-			# If the next values is less than previous, interchange them
-			if unsortedArray[j] > unsortedArray[j+1]:
+            if sortingType == "Ascending" or sortingType == "ascending" or sortingType == "asc" or sortingType == "Asc":
+                # If the next values is less than previous, interchange them
+                if unsortedArray[j] > unsortedArray[j + 1]:
+                    unsortedArray[j], unsortedArray[j + 1] = unsortedArray[j + 1], unsortedArray[j]
+            else:
+                # If the next values is greater than previous, interchange them
+                if unsortedArray[j] < unsortedArray[j + 1]:
+                    unsortedArray[j], unsortedArray[j + 1] = unsortedArray[j + 1], unsortedArray[j]
 
-				unsortedArray[j],unsortedArray[j+1] = unsortedArray[j+1],unsortedArray[j]
+    # Call printArray function to print the sorted array
+    printArray(unsortedArray)
 
-	# Call printArray function to print the sorted array
-	printArray(unsortedArray)
 
 # Function to print Sorted Array
 def printArray(sortedArray):
+    string = ""
 
-	string = ""
+    for i in range(len(sortedArray)):
+        string += str(sortedArray[i]) + " "
 
-	for i in range(len(sortedArray)):
-		string += str(sortedArray[i]) + " "
+    # Print Sorted array to the console
+    print(string)
 
-	# Print Sorted array to the console
-	print(string)
 
 # Main function that executes when script runs on the console.
 if __name__ == "__main__":
-
-	# Test values for sorting
+    # Test values for sorting
     unsortedArray = [64, 34, 25, 12, 22, 11, 90]
 
+    # Type of sorting to be performed Ascending or Descending
+    sortingType = "asc"
+
     # Calling bubble sort function
-    bubbleSort(unsortedArray)
+    bubbleSort(unsortedArray, sortingType)
