8e28e1850d416fd7bce5ed593d4780c51a1ca778
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2019-01-10 15:04:04 -0500

    Implementation of bubble sort in python 3

--- Sorting Algorithms/BubbleSort.py
+++ Sorting Algorithms/BubbleSort.py
@@ -11,51 +11,63 @@
 #
 # **********************************************************************;
 
-# Function that implements Bubble sort
-def bubbleSort(unsortedArray, sortingType, sortingTypeArray):
 
-    # get length of the array
-    arrayLength = len(unsortedArray)
+class BubbleSort:
 
-    # Loop through all elements of the array
-    for i in range(arrayLength):
+    def __init__(self):
 
-        # inner loop to compare with every other elements
-        for j in range(arrayLength - i - 1):
+        # Test values for sorting
+        self.unsortedArray = [54, 89, 43, 36, 34, 100, 56, 39, 56, 31]
 
-            if (sortingType in sortingTypeArray):
-                # If the next values is less than previous, interchange them
-                if unsortedArray[j] > unsortedArray[j + 1]:
-                    unsortedArray[j], unsortedArray[j + 1] = unsortedArray[j + 1], unsortedArray[j]
-            else:
-                # If the next values is greater than previous, interchange them
-                if unsortedArray[j] < unsortedArray[j + 1]:
-                    unsortedArray[j], unsortedArray[j + 1] = unsortedArray[j + 1], unsortedArray[j]
+        self.sortingTypeArray = ["Ascending", "ascending", "asc", "Asc"]
 
-    # Call printArray function to print the sorted array
-    printArray(unsortedArray)
+    # Function that implements Bubble sort
+    def bubbleSort(self, sortingType):
 
+        unsortedArray = self.unsortedArray
 
-# Function to print Sorted Array
-def printArray(sortedArray):
-    string = ""
+        sortingTypeArray = self.sortingTypeArray
 
-    for i in range(len(sortedArray)):
-        string += str(sortedArray[i]) + " "
+        # get length of the array
+        arrayLength = len(unsortedArray)
 
-    # Print Sorted array to the console
-    print(string)
+        # Loop through all elements of the array
+        for i in range(arrayLength):
+
+            # inner loop to compare with every other elements
+            for j in range(arrayLength - i - 1):
+
+                if (sortingType in sortingTypeArray):
+                    # If the next values is less than previous, interchange them
+                    if unsortedArray[j] > unsortedArray[j + 1]:
+                        unsortedArray[j], unsortedArray[j + 1] = unsortedArray[j + 1], unsortedArray[j]
+                else:
+                    # If the next values is greater than previous, interchange them
+                    if unsortedArray[j] < unsortedArray[j + 1]:
+                        unsortedArray[j], unsortedArray[j + 1] = unsortedArray[j + 1], unsortedArray[j]
+
+        # Call printArray function to print the sorted array
+        self.printArray(unsortedArray)
+
+    # Function to print Sorted Array
+    def printArray(self, sortedArray):
+        string = ""
+
+        for i in range(len(sortedArray)):
+            string += str(sortedArray[i]) + " "
+
+        # Print Sorted array to the console
+        print(string)
 
 
 # Main function that executes when script runs on the console.
 if __name__ == "__main__":
-    # Test values for sorting
-    unsortedArray = [64, 34, 25, 12, 22, 11, 90]
-
-    sortingTypeArray = ["Ascending", "ascending", "asc", "Asc"]
 
     # Type of sorting to be performed Ascending or Descending
-    sortingType = "desc"
+    sortingType = "asc"
+
+    # Create object of bubble sort class
+    bubble = BubbleSort()
 
     # Calling bubble sort function
-    bubbleSort(unsortedArray, sortingType, sortingTypeArray)
+    bubble.bubbleSort(sortingType)
