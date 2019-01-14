a70d51cd7b601148f70dcf8bb61a892c156098a0
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2019-01-14 12:52:48 -0500

    Implementation of Linear Search in python 3

--- Searching Algorithms/LinearSearch.py
+++ Searching Algorithms/LinearSearch.py
@@ -11,34 +11,49 @@
 #
 # **********************************************************************;
 
-# Function that implements Linear Search algorithm
-def LinearSearch(searchArray, searchedValue):
+class LinearSearch:
+    searchArray = []
+    searchedValue = 0
+    arraySize = 0
 
-    # Loop through all the elements in the array for searched value
-    for i in range(len(searchArray)):
+    def __init__(self):
 
-        # If searched value is found at an index return the index
-        if searchArray[i] == searchedValue:
-            return i
-    # If the searched value is not present in the array return -1 as the index
-    return -1
+        self.searchArray = [54, 89, 43, 36, 34, 100, 56, 39, 12, 31]
 
-# Function that prints the index of the element
-def printIndex(index):
-    
-    # Check for the value of the index
-    if index == -1:
-        print("Value was not found in the array")
-    else:
-        print("Value was found at index : " + str(index))
+        self.searchedValue = 100
+
+        self.arraySize = len(self.searchArray)
+
+    def printIndex(self, index):
+
+        # Check for the value of the index
+        if index == -1:
+            print("Value was not found in the array")
+        else:
+            print("Value was found at index : " + str(index))
+
+    # Function that implements Linear Search algorithm
+    def linearSearch(self):
+
+        searchArray = self.searchArray
+
+        searchedValue = self.searchedValue
+
+        # Loop through all the elements in the array for searched value
+        for i in range(len(searchArray)):
+
+            # If searched value is found at an index return the index
+            if searchArray[i] == searchedValue:
+                return i
+        # If the searched value is not present in the array return -1 as the index
+        return -1
+
 
 # Main function that executes when script runs on the console.
 if __name__ == "__main__":
-    # Initialize searching array
-    searchArray = [2, 3, 4, 10, 40]
 
-    # Value to be searched in the array
-    searchedValue = 4
+    linear = LinearSearch()
 
-    # Call Linear Search function and print the index
-    printIndex(LinearSearch(searchArray, searchedValue))
+    index = linear.linearSearch()
+
+    linear.printIndex(index)
