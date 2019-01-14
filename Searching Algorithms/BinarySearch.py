b9af173ab08bebd107770e043444c76efd77197a
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2019-01-14 12:51:14 -0500

    Implementation of Binary Search in python 3

--- Searching Algorithms/BinarySearch.py
+++ Searching Algorithms/BinarySearch.py
@@ -11,52 +11,65 @@
 #
 # **********************************************************************;
 
-# Function that uses binary search
-def BinarySearch(searchArray, leftLimit, rightLimit, searchedValue):
+class BinarySearch:
 
-    # Check base case
-    if rightLimit >= leftLimit:
+    searchArray = []
+    searchedValue = 0
+    arraySize = 0
 
-        middleElement = leftLimit + (rightLimit - leftLimit) / 2
+    def __init__(self):
 
-        # If element is present at the middle itself
-        if searchArray[middleElement] == searchedValue:
-            return middleElement
+        self.searchArray = [54, 89, 43, 36, 34, 100, 56, 39, 12, 31]
 
-        # If element is smaller than mid, then it
-        # can only be present in left subarray
-        elif searchArray[middleElement] > searchedValue:
-            return BinarySearch(searchArray, leftLimit, middleElement - 1, searchedValue)
+        self.searchedValue = 56
 
-        # Else the element can only be present
-        # in right subarray
+        self.arraySize = len(self.searchArray)
+
+
+    def printIndex(self, index):
+
+        # Check for the value of the index
+        if index == -1:
+            print("Value was not found in the array")
         else:
-            return BinarySearch(searchArray, middleElement + 1, rightLimit, searchedValue)
+            print("Value was found at index : " + str(index))
 
-    else:
-        # Element is not present in the array
-        return -1
+    # Function that uses binary search
+    def binarySearch(self, leftLimit, rightLimit):
 
-# Function that prints the index of the element
-def printIndex(index):
-    
-    # Check for the value of the index
-    if index == -1:
-        print("Value was not found in the array")
-    else:
-        print("Value was found at index : " + str(index))
+        searchedValue = self.searchedValue
+
+        searchArray = self.searchArray
+
+        # Check base case
+        if rightLimit >= leftLimit:
+
+            middleElement = leftLimit + (rightLimit - leftLimit) / 2
+
+            # If element is present at the middle itself
+            if searchArray[middleElement] == searchedValue:
+                return middleElement
+
+            # If element is smaller than mid, then it
+            # can only be present in left subarray
+            elif searchArray[middleElement] > searchedValue:
+                return self.binarySearch(leftLimit, middleElement - 1)
+
+            # Else the element can only be present
+            # in right subarray
+            else:
+                return self.binarySearch(middleElement + 1, rightLimit)
+
+        else:
+            # Element is not present in the array
+            return -1
 
 
 # Main function that executes when script runs on the console.
 if __name__ == "__main__":
-    # Initialize searching array
-    searchArray = [2, 3, 4, 10, 40]
 
-    # Length of Search Array
-    arrayLength = len(searchArray)
+    binary = BinarySearch()
 
-    # Value to be searched in the array
-    searchedValue = 10
+    index = binary.binarySearch(0, binary.arraySize - 1)
 
-    # Call Binary Search function and print the index
-    printIndex(BinarySearch(searchArray, 0, arrayLength, searchedValue))
+    binary.printArray(index)
