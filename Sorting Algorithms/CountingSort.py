4b1509cf10e57765fca6e379ca2bc1d682008d3f
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2019-01-15 13:43:54 -0500

    Implementation of Counting Sort in python 3

--- Sorting Algorithms/CountingSort.py
+++ Sorting Algorithms/CountingSort.py
@@ -11,66 +11,78 @@
 #
 # **********************************************************************;
 
-# A function to do counting sort of arr[] according to the digit represented by exponent.
-def countingSort(unsortedArray, exponent):
+class CountingSort:
 
-    arraySize = len(unsortedArray)
+    def __init__(self):
 
-    # The output array elements that will have sorted arr
-    output = [0] * arraySize
+        # Test values for sorting
+        self.unsortedArray = [170, 45, 75, 90, 802, 24, 2, 66]
 
-    # initialize count array as 0
-    count = [0] * 10
+        self.max1 = max(self.unsortedArray)
 
-    # Store count of occurrences in count[]
-    for i in range(0, arraySize):
-        index = int(unsortedArray[i] / exponent)
-        count[index % 10] += 1
+        self.sortingTypeArray = ["Ascending", "ascending", "Asc", "asc"]
 
-    # Change count[i] so that count[i] now contains actual position of this digit in output array
-    for i in range(1, 10):
-        count[i] += count[i - 1]
+    # A function to do counting sort of arr[] according to the digit represented by exponent.
+    def countingSort(self, unsortedArray, exponent, sortingType):
 
-    # Build the output array
-    i = arraySize - 1
-    while i >= 0:
-        index = int(unsortedArray[i] / exponent)
-        output[count[index % 10] - 1] = unsortedArray[i]
-        count[index % 10] -= 1
-        i -= 1
+        arraySize = len(unsortedArray)
 
-    # Copying the output array to arr[],so that arr now contains sorted numbers
-    i = 0
-    for i in range(0, len(unsortedArray)):
-        unsortedArray[i] = output[i]
+        # The output array elements that will have sorted arr
+        output = [0] * arraySize
 
+        # initialize count array as 0
+        count = [0] * 10
 
+        # Store count of occurrences in count[]
+        for i in range(0, arraySize):
+            index = int(unsortedArray[i] / exponent)
+            count[index % 10] += 1
 
-# Function to print Sorted Array
-def printSortedArray(sortedArray):
+        # Change count[i] so that count[i] now contains actual position of this digit in output array
+        for i in range(1, 10):
+            count[i] += count[i - 1]
 
-    string = ""
+        # Build the output array
+        i = arraySize - 1
+        while i >= 0:
+            index = int(unsortedArray[i] / exponent)
+            output[count[index % 10] - 1] = unsortedArray[i]
+            count[index % 10] -= 1
+            i -= 1
 
-    for i in range(len(sortedArray)):
-        string += str(sortedArray[i]) + " "
+        # Copying the output array to arr[],so that arr now contains sorted numbers
 
-    # Print Sorted array to the console
-    print(string)
+        if sortingType in self.sortingTypeArray:
+            for i in range(0, len(unsortedArray)):
+                unsortedArray[i] = output[i]
+        else:
+            for i in range(len(unsortedArray)-1, -1, -1):
+                unsortedArray[i] = output[i]
+
+    # Function to print Sorted Array
+    def printArray(self, sortedArray):
+
+        string = ""
+
+        for i in range(len(sortedArray)):
+            string += str(sortedArray[i]) + " "
+
+        # Print Sorted array to the console
+        print(string)
 
 
 # Main function that executes when script runs on the console.
 if __name__ == "__main__":
 
-    # Test values for sorting
-    unsortedArray = [ 170, 45, 75, 90, 802, 24, 2, 66]
+    sortingType = "desc"
 
-    max1 = max(unsortedArray)
+    count = CountingSort()
 
     # Calling insertion sort function
     exponent = 1
-    while max1 / exponent > 0:
-        countingSort(unsortedArray, exponent)
+    while count.max1 / exponent > 0:
+        count.countingSort(count.unsortedArray, exponent, sortingType)
         exponent *= 10
 
     # Call print function to print sorted array
-    printSortedArray(unsortedArray)
+    count.printArray(count.unsortedArray)
