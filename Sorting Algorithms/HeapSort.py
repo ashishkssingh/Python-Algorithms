552be84ca8efab1806200ed5a3f2bf8ba631c8f0
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2019-01-15 14:00:34 -0500

    Implementation of Heap Sort in python 3

--- Sorting Algorithms/HeapSort.py
+++ Sorting Algorithms/HeapSort.py
@@ -5,70 +5,92 @@
 #
 # Author            : Ashish Singh
 #
-# Date created      : 20190104
+# Date created      : 20190115
 #
 # Purpose           : Implementing heap sort algorithm in python
 #
 # **********************************************************************;
 
-# To heapify subtree rooted at index i
-def heapify(unsortedArray, arraySize, i):
+class HeapSort:
 
-    largest = i  # Initialize largest as root
-    left = 2 * i + 1  # left node = 2*i + 1
-    right = 2 * i + 2  # right node= 2*i + 2
+    def __init__(self):
 
-    # See if left child of root exists and is greater than root
-    if left < arraySize and unsortedArray[i] < unsortedArray[left]:
-        largest = left
+        self.unsortedArray = [18, 75, 77, 70, 32, 15, 71, 64, 67, 14]
 
-    # See if right child of root exists and is greater than root
-    if right < arraySize and unsortedArray[largest] < unsortedArray[right]:
-        largest = right
+        self.arraySize = len(self.unsortedArray)
 
-    # Change root, if needed
-    if largest != i:
+        self.sortingTypeArray = ["Ascending", "ascending", "Asc", "asc"]
 
-        # Swap the elements
-        unsortedArray[i], unsortedArray[largest] = unsortedArray[largest], unsortedArray[i]
+    # To heapify subtree rooted at index i
+    def heapify(self, unsortedArray, arraySize, i, sortingType):
 
-        # Heapify the root node
-        heapify(unsortedArray, arraySize, largest)
+        swapValue = i  # Initialize largest as root
+        left = 2 * i + 1  # left node = 2*i + 1
+        right = 2 * i + 2  # right node= 2*i + 2
 
-# The main function to sort an array of given size
-def heapSort(unsortedArray):
-    arraySize = len(unsortedArray)
+        if sortingType in self.sortingTypeArray:
 
-    # Build a maxheap
-    for i in range(arraySize, -1, -1):
-        heapify(unsortedArray, arraySize, i)
+            # See if left child of root exists and is greater than root
+            if left < arraySize and unsortedArray[i] < unsortedArray[left]:
+                swapValue = left
 
-    # One by one extract elements from array and put it into heap
-    for i in range(arraySize - 1, 0, -1):
+            # See if right child of root exists and is greater than root
+            if right < arraySize and unsortedArray[swapValue] < unsortedArray[right]:
+                swapValue = right
 
-        # Swap the elements
-        unsortedArray[i], unsortedArray[0] = unsortedArray[0], unsortedArray[i]
-        heapify(unsortedArray, i, 0)
+        else:
 
-# Function to print Sorted Array
-def printSortedArray(sortedArray):
-    string = ""
+            # See if left child of root exists and is less than root
+            if left < arraySize and unsortedArray[i] > unsortedArray[left]:
+                swapValue = left
 
-    for i in range(len(sortedArray)):
-        string += str(sortedArray[i]) + " "
+            # See if right child of root exists and is less than root
+            if right < arraySize and unsortedArray[swapValue] > unsortedArray[right]:
+                swapValue = right
 
-    # Print Sorted array to the console
-    print(string)
+        # Change root, if needed
+        if swapValue != i:
+            # Swap the elements
+            unsortedArray[i], unsortedArray[swapValue] = unsortedArray[swapValue], unsortedArray[i]
+
+            # Heapify the root node
+            self.heapify(unsortedArray, arraySize, swapValue, sortingType)
+
+    # The main function to sort an array of given size
+    def heapSort(self, unsortedArray, sortingType):
+        arraySize = len(unsortedArray)
+
+        # Build a maxheap
+        for i in range(arraySize, -1, -1):
+            self.heapify(unsortedArray, arraySize, i, sortingType)
+
+        # One by one extract elements from array and put it into heap
+        for i in range(arraySize - 1, 0, -1):
+            # Swap the elements
+            unsortedArray[i], unsortedArray[0] = unsortedArray[0], unsortedArray[i]
+            self.heapify(unsortedArray, i, 0, sortingType)
+
+    # Function to print Sorted Array
+    def printArray(self, sortedArray):
+        string = ""
+
+        for i in range(len(sortedArray)):
+            string += str(sortedArray[i]) + " "
+
+        # Print Sorted array to the console
+        print(string)
 
 
 # Main function that executes when script runs on the console.
 if __name__ == "__main__":
 
-    # Test values for sorting
-    unsortedArray = [12, 11, 13, 5, 6]
+    sortingType = "desc"
+
+    # Create object for the class
+    heap = HeapSort()
 
     # Calling insertion sort function
-    heapSort(unsortedArray)
+    heap.heapSort(heap.unsortedArray, sortingType)
 
     # Print the sorted array
-    printSortedArray(unsortedArray)
+    heap.printArray(heap.unsortedArray)
